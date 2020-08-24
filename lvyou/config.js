1a3821e4ed4823be17f925aee824e7774ab0c5e5
zhangxianfei <824026951@qq.com>
2020-08-24 16:38:08 +0800

    旅游小程序

--- lvyou/config.js
+++
@@ -1,7 +0,0 @@
-const config={
-  // api_blink_url:"https://callme.lixinapp.com/weixiu/api/service",
-  api_blink_url:"https://xcx.huandugj.com/tourism/api/service",
-  // api_blink_url:"http://192.168.3.198:8080/tourism/api/service",
-  appkey:"wx86e0deb09dbd8a5c"
-}
-module.exports=config;
